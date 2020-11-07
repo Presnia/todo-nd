8229dea401b8e89396ee52e9f2cf8ec7c7fbee58
Andrei Dzebysh <presnia@yahoo.com>
2020-11-07 22:48:33 +0300

    Step 2 is finished

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ if (localStorage.getItem('todo')) {
   displayMessages();
 }
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', () => {
 
     let newTodo = {
       todo: addMessage.value,
@@ -20,25 +20,45 @@ addButton.addEventListener('click', function() {
     todoList.push(newTodo);
     displayMessages();
     localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
 });
 
 function displayMessages() {
     let displayMessage = '';
-    
-    todoList.forEach(function(item, i){
+    if (todoList.length === 0) todo.innerHTML = '';
+    todoList.forEach((item, i) => {
         displayMessage += `
         <li>
           <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-          <label for="item_${i}">${item.todo}</label>
+          <label for="item_${i}"  class='${item.important ? 'important' : ''}'>${item.todo}</label>
         </li>
         `;
         todo.innerHTML = displayMessage; 
     });
 };
 
-todo.addEventListener('change', function(event) {
-    let idInput = event.target.getAttribute('id');
-    let forLabel = todo.querySelector('[for=' + idInput + ']');
-    let valueLabel = forLabel.innerHTML;
-    console.log(valueLabel);
+todo.addEventListener('change', (event) => {
+    let valueLabel = todo.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
+    
+    todoList.forEach((item) => {
+      if (item.todo === valueLabel){
+        item.checked = !item.checked;
+        localStorage.setItem('todo', JSON.stringify(todoList));
+      }
+    })
+});
+
+todo.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+    todoList.forEach((item, i) => {
+        if(item.todo === event.target.innerHTML){
+          if(event.cmndKey || event.metaKey){
+            todoList.splice(i, 1);
+          } else {
+            item.important = !item.important;
+          }
+          displayMessages();
+          localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
 });
